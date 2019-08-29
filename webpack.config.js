bc3441df34307d41efad99bce8a8efa6e41849a3
Uttam Khanal <uttam_khanal12@hotmail.com>
2019-08-29 22:08:23 +0545

    add prod modes

--- webpack.config.js
+++ webpack.config.js
@@ -8,62 +8,64 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
     inject: 'body'
 });
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve('dist'),
-        filename: 'index_bundle.js',
-        publicPath: '/'
-    },
-    node : {
-        __filename: true,
-        __dirname: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|png|jpg|gif)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {}
-                    }
-                ]
-            },
-        ]
-    },
-    devServer: {
-        stats: {
-            hash: false,
-            version: false,
-            timings: true,
-            assets: false,
-            chunks: false,
-            modules: false,
-            reasons: false,
-            children: false,
-            source: false,
-            errors: true,
-            errorDetails: true,
-            warnings: true,
-            publicPath: false
+module.exports = (env, argv) => {
+    return {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve('dist'),
+            filename: 'index_bundle.js',
+            publicPath: env && env.mode === 'production' ? './' : '/'
         },
-        historyApiFallback: true,
-    },
-    plugins: [HtmlWebpackPluginConfig]
+        node : {
+            __filename: true,
+            __dirname: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        {
+                            loader: 'style-loader'
+                        },
+                        {
+                            loader: 'css-loader'
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.(eot|svg|ttf|woff|png|jpg|gif)$/,
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {}
+                        }
+                    ]
+                },
+            ]
+        },
+        devServer: {
+            stats: {
+                hash: false,
+                version: false,
+                timings: true,
+                assets: false,
+                chunks: false,
+                modules: false,
+                reasons: false,
+                children: false,
+                source: false,
+                errors: true,
+                errorDetails: true,
+                warnings: true,
+                publicPath: false
+            },
+            historyApiFallback: true,
+        },
+        plugins: [HtmlWebpackPluginConfig]
+    };
 };
